42a3b497983a889848c94eeb327345726b695467
Pavel Gri <zlibabaika@gmail.com>
2023-01-29 21:28:01 +0400

    start setting project, add image, story

---
+++ Destuni-Angela-Homework/Application/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  Destuni-Angela-Homework
+//
+//  Created by Павел Грицков on 29.01.23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ Destuni-Angela-Homework/Model/Story.swift
@@ -0,0 +1,16 @@
+//
+//  Story.swift
+//  Destuni-Angela-Homework
+//
+//  Created by Павел Грицков on 29.01.23.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
